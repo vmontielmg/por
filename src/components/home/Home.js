9c89ccfcfbae1d83c647270bb7d213145afafd49
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2022-05-05 10:20:31 -0700

    add styling to home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,20 @@ class Home extends React.Component {
     render () {
         return (
             <div className='Home'>
-                <h1> HOME </h1>
-                <p>Hola! Welcome to my Portfolio :) <br />
-                My name is Vanessa and I am a Software Engineer based in Los Angeles, CA <br />
-                Currently learning React (what this portfolio was built with) but knowledgeable in: <br />
-                Java, C#, HTML/CSS, JavaScript, Object-Oriented Programming and eager to learn more! <br />
-                </p>
+                <div className='img-cluster' id='column'>
+                    <img src={require('../../images/vmontiel-icon.jpg')} alt='headshot of woman' className='self-portrait'></img>
+                    {/* <img src={require('../../images/vmontiel-icon.jpg')} alt='headshot of woman' style={{height:'150px'}}></img>
+                    <img src={require('../../images/vmontiel-icon.jpg')} alt='headshot of woman' style={{height:'150px'}}></img> */}
+                </div>
+                <div className='intro' id='column'>
+                    <h1 id='name'> Vanessa Montiel </h1>
+                    <p>
+                        &#128105; Hi, my name is Vanessa <br/>
+                        &#127891; B.S. Computer Science CSULB '21 <br/>
+                        &#128205; Los Angeles, CA <br/>
+                        &#128187; Software Engineer  <br/>
+                    </p>
+                </div>
             </div>
         )
     }
