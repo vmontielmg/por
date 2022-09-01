f1b5813713e18f4c8a20af440e5d14a213c0da73
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2022-09-01 16:28:35 -0700

    Added contact links and styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,25 @@
 import React from "react";
+import '../../App.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faEnvelope, faGithub, faLinkedin);
 
 class Contact extends React.Component {
     render() {
         return (
             <div className="Contact">
-                <h1>Contact Me</h1>
+                <h1>Get in touch with me!</h1> 
+                <p className="contactDescription">
+                    Feel free to reach out to me through any of the listed platforms and I will try to get back to you as soon as possible!
+                </p>
+                <div className="contactLinks">
+                    <a href="mailto:vmontielmg@gmail.com" target="_blank"><FontAwesomeIcon icon="envelope" /></a>
+                    <a href="https://www.linkedin.com/in/montiel-vanessa/" target="_blank"> <FontAwesomeIcon icon={faLinkedin} /> </a>
+                    <a href="https://github.com/vmontielmg" target="_blank"><FontAwesomeIcon icon={faGithub} /></a> 
+                </div>
             </div>
 
         );
