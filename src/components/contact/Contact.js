e4aa290617f28bfe7665a9645808de600838882d
Vanessa Montiel <32447570+vmontielmg@users.noreply.github.com>
2022-09-01 17:26:08 -0700

    fix pls

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../../App.css';
-import { FontAwesomeIcon } from "@fontawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
